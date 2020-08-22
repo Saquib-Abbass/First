6ffd005c988d6774fd9bab29e963a8378aec7bdc
Saquib-Abbass <70056035+Saquib-Abbass@users.noreply.github.com>
2020-08-22 14:21:05 +0500

    Add files via upload

---
+++ chp12.1.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+lst = []
+total = 0
+# Retrieve all of the anchor tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
+    lst.append(tag.contents[0])
+
+for i in lst:
+    num = int(i)
+    total = total + num
+print(total)
